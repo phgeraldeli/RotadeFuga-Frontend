43944ebec0eca5191b553208194cb2dd1661e9e3
Pedro Cotrim <pacotrim@gmail.com>
2019-11-27 00:14:57 -0300

    Adicionando polyline

--- lib/Screens/mapa.dart
+++ lib/Screens/mapa.dart
@@ -1,7 +1,25 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
 
+
+class Rota {
+  final List<LatLng> pontos;
+  Rota({this.pontos});
+  factory Rota.fromJson(List<dynamic> json) {
+    Map<String, dynamic> data = json[0];
+    List<dynamic> pts = data['pontos'];
+    List<LatLng> temp = new List();
+    for(Map<String, dynamic> pt in pts)
+    {
+      temp.add(LatLng(pt['latitude'].toDouble(),(pt['longitude']).toDouble()));
+    }
+    print(temp.toString());
+    return Rota( pontos: temp);
+  }
+}
 
 class Mapa extends StatefulWidget {
   @override
@@ -10,13 +28,31 @@ class Mapa extends StatefulWidget {
   }
 }
 
+Future<Rota> getRota() async {
+  final response = await http.get('https://teste-nodejs-rota.herokuapp.com/rotas/5ddddffabe21b30022a06fa7');
+  if (response.statusCode == 200){
+    return Rota.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('Falha na requisição');
+  }
+}
+
 class MapaState extends State<Mapa> {
   GoogleMapController mapController;
   Geolocator locate = Geolocator();
   LatLng _center = LatLng(-22.905336, -43.141555);
+  Future<Rota> _rota;
+  final Set<Polyline> _polyline={};
 
   void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
+        mapController = controller;
+        _rota.then((data)=>{setState(() => { _polyline.add(Polyline(polylineId: PolylineId("Rota Default"), visible: true, points:data.pontos, color: Colors.blue))})});
+    }
+
+  @override
+  void initState(){
+    super.initState();
+    _rota = getRota();
   }
 
   @override
@@ -28,6 +64,7 @@ class MapaState extends State<Mapa> {
           backgroundColor: Colors.deepPurple,
         ),
         body: GoogleMap(
+          polylines: _polyline,
           onMapCreated: _onMapCreated,
           initialCameraPosition: CameraPosition(
             target: _center,
