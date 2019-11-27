ab8f739303f6684dc3b16364122cfa9b396c869d
Pedro Cotrim <pacotrim@gmail.com>
2019-11-27 00:03:58 -0300

    Adiciona consulta de rota no back e desenho no mapa

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,25 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+
+class Rota {
+  final List<LatLng> pontos;
+  Rota({this.pontos});
+  factory Rota.fromJson(List<dynamic> json) {
+    Map<String, dynamic> data = json[0];
+    List<dynamic> pts = data['pontos'];
+    List<LatLng> temp = new List();
+    for(Map<String, dynamic> pt in pts)
+    {
+      temp.add(LatLng(pt['latitude'].toDouble(),(pt['longitude']).toDouble()));
+    }
+    print(temp.toString());
+    return Rota( pontos: temp);
+  }
+}
 
 void main() => runApp(MyApp());
 
@@ -172,13 +191,31 @@ class MapsPage extends StatefulWidget {
   _MapsPageState createState() => _MapsPageState();
 }
 
+Future<Rota> getRota() async {
+  final response = await http.get('https://teste-nodejs-rota.herokuapp.com/rotas/5dddc30b8eaf474c34c64ea8');
+  if (response.statusCode == 200){
+    return Rota.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('Falha na requisição');
+  }
+}
+
 class _MapsPageState extends State<MapsPage> {
   GoogleMapController mapController;
   Geolocator locate = Geolocator();
   LatLng _center = LatLng(-22.905336, -43.141555);
+  Future<Rota> _rota;
+  final Set<Polyline> _polyline={};
 
   void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
+        mapController = controller;
+        _rota.then((data)=>{setState(() => { _polyline.add(Polyline(polylineId: PolylineId("Rota Default"), visible: true, points:data.pontos, color: Colors.blue))})});
+    }
+
+  @override
+  void initState(){
+    super.initState();
+    _rota = getRota();
   }
 
   @override
@@ -190,6 +227,7 @@ class _MapsPageState extends State<MapsPage> {
           backgroundColor: Colors.blue,
         ),
         body: GoogleMap(
+          polylines: _polyline,
           onMapCreated: _onMapCreated,
           initialCameraPosition: CameraPosition(
             target: _center,
